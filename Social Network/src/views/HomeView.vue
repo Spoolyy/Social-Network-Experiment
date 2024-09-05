bdf175f8ae30f16609df40b0d28c0f5055668b6e
Spoolyy <alessiosimeone3@gmail.com>
2024-09-05 19:07:40 +0200

    First upload
    Uploaded local project before development start

---
+++ Social Network/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <h1 class="text-blue-700 text-3xl">HELLOOOOOOOOO</h1>
+  </main>
+</template>
