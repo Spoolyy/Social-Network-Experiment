d4e5ecaaeb7735818be5a531eff1cd0dff2db7e5
Spoolyy <alessiosimeone3@gmail.com>
2024-10-18 20:23:33 +0200

    temp

---
+++ Social Network/src/components/Toast.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {useToastStore} from '@/stores/toast'
+
+const store = useToastStore()
+
+return {
+    store
+}
+</script>
+<template>
+    <div v-if="store.isVisible"
+    class="transitione ease-in-out delay-500 duration-500 p-6 fixed top-full right-8 rounded-xl shadow-xl"
+    :class="store.classes">
+        {{ store.message }}
+    </div>
+</template>
